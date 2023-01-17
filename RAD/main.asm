edbe91869fdf4641ef8f9d437f9d9bda58789d4d
Gorka Suárez <alexander.lessman@gmail.com>
2023-01-17 02:59:01 +0100

    C64 hello world test

---
+++ RAD/main.asm
@@ -0,0 +1,36 @@
+;===============================================================================
+; Copyright (c) 2023 Gorka Suárez
+;
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+;
+; The above copyright notice and this permission notice shall be included in
+; all copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+; SOFTWARE.
+;===============================================================================
+*=$0801 ; 10 SYS2064
+        BYTE    $0B, $08, $0A, $00, $9E, $32, $30, $36, $34, $00, $00, $00
+
+*=$0810
+main    jsr $E544
+        ldx #$00
+@loop   lda message,x
+        beq @end
+        jsr $E716
+        inx
+        jmp @loop
+@end    rts
+
+message TEXT "hello, world!"
+        BYTE 13, 0
